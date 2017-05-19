a32fe4074893fae0ac94c07018e56e90547b3b9d
Max Bruckner <max@maxbruckner.de>
2017-05-19 13:24:47 +0200

    CI: Run normal tests with meson

---
+++ ci/meson.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+[ ! -e mesonbuild ] && mkdir build
+cd mesonbuild || exit 1
+
+rm -r -- *
+if meson .. -Denable_bindings=true -Denable_docs=true -Db_lto=false; then
+    # This has to be done with else because with '!' it won't work on Mac OS X
+    echo
+else
+    exit $? #abort on failure
+fi
+
+mesontest
